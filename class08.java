37db5f3e66e41852d2fb1cd54636d9dea22cf469
wufeimeng <113487917+wufeimeng@users.noreply.github.com>
2022-12-01 16:55:41 +0800

    Add files via upload

---
+++ class08.java
@@ -0,0 +1,12 @@
+package java12;
+
+public class class08
+ {
+ public static void main(String args[])
+ {
+ String str="262904713";
+ int num;
+  num=Integer.parseInt(str);
+  System.out.println(num+"*2="+num*2);
+  }
+  }
