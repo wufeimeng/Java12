a1a82002a8c7d1ddc9cb398a7c292b947b1c52a6
wufeimeng <113487917+wufeimeng@users.noreply.github.com>
2022-12-01 16:51:55 +0800

    Add files via upload

---
+++ class08.java
@@ -0,0 +1,11 @@
+package java12;
+
+public class class08
+ {
+ public static void main(String args[])
+ {String str="Habit is second nature.";
+ System.out.println("string="+str);
+ System.out.println("sub-string="+str.substring(16,22));
+ }
+ }
+
