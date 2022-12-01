3b92e8577c09a4a49384fd128757a56737c2b185
wufeimeng <113487917+wufeimeng@users.noreply.github.com>
2022-12-01 16:53:13 +0800

    Add files via upload

---
+++ class08.java
@@ -0,0 +1,13 @@
+package java12;
+
+public class class08
+ {
+ public static void main(String args[])
+ {
+ String str;
+ str=Float.toString(123.45f);
+ System.out.println(str);
+ System.out.println("length="+str.length());
+ }
+ }
+
